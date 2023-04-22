cf421149fd82b9c80bdd341004ff754efcb572b5
matrix920 <matrix.leader.920@gmail.com>
2023-04-22 11:40:09 +0400

    Invokers

---
+++ AltkamulAlteqani.Invokers/Invokers/BaseInvoker.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Net.Http;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using Newtonsoft.Json;
+using AltkamulAlteqani.Entities.ApiModels;
+
+namespace AltkamulAlteqani.Invokers.Invokers
+{
+    public class BaseInvoker<TEntity, TEntityOut> where TEntity : class where TEntityOut : class
+    {
+        public static async Task<List<TEntityOut>> Get(string request)
+        {
+            string BaseApi = ConfigurationManager.AppSettings.Get("StackoverflowApi");
+            string urlParameter = request;
+
+            HttpClientHandler handler = new HttpClientHandler();
+            handler.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+            try
+            {
+                using (var httpClient = new HttpClient(handler))
+                {
+                    httpClient.BaseAddress = new Uri(BaseApi);
+                    httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                    var response = await httpClient.GetAsync(urlParameter);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var res = response.Content.ReadAsStringAsync().Result;
+                        var dataObjects = JsonConvert.DeserializeObject<ApiResponse<TEntityOut>>(res);
+                        return dataObjects.items;
+                    }
+                    else if (response.StatusCode == HttpStatusCode.BadRequest)
+                    {
+                        var stringMesg = response.Content.ReadAsStringAsync().Result;
+                        var errorMessage = JsonConvert.DeserializeObject<dynamic>(stringMesg);
+                        throw new InvalidDataException(errorMessage);
+                    }
+                    else
+                    {
+                        throw new Exception(response.ToString());
+                    }
+                }
+            }
+            catch (InvalidDataException Inex)
+            {
+                throw Inex;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        
+    }
+}
---
+++ AltkamulAlteqani.Invokers/StackOverFlowInvokers/QuestionInvoker.cs
@@ -0,0 +1,25 @@
+﻿using AltkamulAlteqani.Entities.ApiModels;
+using AltkamulAlteqani.Invokers.Invokers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AltkamulAlteqani.Invokers.StackOverFlowInvokers
+{
+    public class QuestionInvoker
+    {
+
+        public static async Task<List<Question>> GetCompressedQuesions(int page = 1, int pageSize = 10, string sort = "creation", string order = "desc", string site = "stackoverflow")
+        {
+            return await BaseInvoker<Question, Question>.Get($"questions?page={page}&pagesize={pageSize}&sort={sort}&order={order}&site={site}");
+
+        }
+
+        public static async Task<List<Question>> GetCompressedQuestion(int questionId)
+        {
+            return await BaseInvoker<Question, Question>.Get($"questions/{questionId}?site=stackoverflow");
+        }
+    }
+}
