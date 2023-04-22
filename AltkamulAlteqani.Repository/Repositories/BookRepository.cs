fa2019fae2081ef8b5d4c7f11f6317409c745162
matrix920 <matrix.leader.920@gmail.com>
2023-04-22 11:45:12 +0400

    Repository

---
+++ AltkamulAlteqani.Repository/Repositories/BookRepository.cs
@@ -0,0 +1,38 @@
+﻿using AltkamulAlteqani.Entities.Core;
+using BaseRepository.Repositories;
+using BaseRepository.UnitOfWork;
+using Repository.EF;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AltkamulAlteqani.Repository.Repositories
+{
+    public interface IBookRepository : IRepositoryAsync<Book>
+    {
+        IEnumerable<Book> GetByTypeId(int? bookTypeId);
+    }
+
+    public class BookRepository : Repository<Book>, IBookRepository
+    {
+        public BookRepository(DbContext context, IUnitOfWorkAsync unitOfWork) : base(context, unitOfWork)
+        {
+        }
+
+        public IEnumerable<Book> GetByTypeId(int? bookTypeId)
+        {
+            var bookTypeIdParameter = bookTypeId.HasValue ?
+                new SqlParameter("@BookTypeId", bookTypeId) :
+                new SqlParameter("@BookTypeId", typeof(int));
+
+            var x = SelectQuery("BooksByType @BookTypeId", bookTypeIdParameter).ToList();
+
+            return x;
+        }
+    }
+}
